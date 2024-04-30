401e78c5ef37bf2da63cc7828e1c99b585a4df6f
Abhay Vishwakarma <118156018+helloiamabhay@users.noreply.github.com>
2024-04-30 08:11:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin:"https://abhayvsk.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
